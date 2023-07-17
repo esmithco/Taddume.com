76e78577b21fada94f6210dd71ccc8e937d85c8a
esmithco <mendozajaimes.6@teleperformance.co>
2023-07-17 13:04:11 -0500

    se pone banner y las materias

--- home/js/script.js
+++ home/js/script.js
@@ -1,11 +1,11 @@
-/* para el json*/
+/* para el json
 import people from "../data/people.json" assert{type:"json"}
 
 const list = document.querySelector('#projects')
 
 people.data.forEach(e => {
 
-    /* div profileEL */
+    /* div profileEL 
     const profileEL = document.createElement('div')
     profileEL.classList.add('profile')
     const imageEl = document.createElement("img")
@@ -13,15 +13,15 @@ people.data.forEach(e => {
     imageEl.alt = e.id
     profileEL.appendChild(imageEl)
 
-    /* titulo nombre */
+    /* titulo nombre 
     const nameEL = document.createElement("h3")
     nameEL.innerHTML = e.name
 
-    /* titulo pais */
+    /* titulo pais 
     const countryEL = document.createElement("h5")
     countryEL.innerHTML = e.country
 
-    /* comentario */
+    /* comentario 
     const commentEL = document.createElement('div')
     commentEL.classList.add('comments')
     const descEL = document.createElement("p")
@@ -34,5 +34,21 @@ people.data.forEach(e => {
     list.appendChild(commentEL)
 
 })
+*/
 
+function changeImage() {
+    document.getElementById('img1').style.display = 'none';
+    document.getElementById('img2').style.display = 'inline-block';
+    img1.style.opacity = '0';
+    img2.style.opacity = '1';
+}
 
+function restoreImage() {
+    document.getElementById('img1').style.display = 'inline-block';
+    document.getElementById('img2').style.display = 'none';
+    img1.style.opacity = '1';
+    img2.style.opacity = '0';
+}
+
+document.getElementById('img1').addEventListener('mouseenter', changeImage);
+document.getElementById('img2').addEventListener('mouseleave', restoreImage);
