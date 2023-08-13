f15dc7dc1df4628f8b9eb573998fc83d3fa1aec0
Brayan Mendoza <smith0mendoza@gmail.com>
2023-08-13 14:02:34 -0500

    se crean los submenus con javascript y se adapta el tamaño del menu-mobile

--- home/js/script.js
+++ home/js/script.js
@@ -2,6 +2,7 @@
 const menuIcon = document.querySelector('.icon-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const navSubjects = document.querySelector('.subjects-learn');
+const ulSubMenus = document.querySelector('.ul-header');
 
 
 menuIcon.addEventListener('click', toggleMobileMenu);
@@ -19,6 +20,7 @@ function hideSubMenu(id) {
 
 function toggleMobileMenu() {
     mobileMenu.classList.toggle('inactive');
+    mobileMenu.classList.toggle('active-menu');
 };
 
 function changeImage(imgId1, imgId2, pId) {
@@ -33,6 +35,117 @@ function restoreImage(imgId1, imgId2, pId) {
     document.getElementById(pId).classList.remove("hover");
 };
 
+function createMenuLinks(arr) {
+    const linksContainer = document.createElement('ul');
+    for (link of arr) {
+        const aLink = document.createElement('a');
+        aLink.setAttribute('href', link.enlHerf)
+        aLink.innerText = link.enlName;
+
+        const liMenu = document.createElement('li');
+        liMenu.appendChild(aLink);
+
+        linksContainer.appendChild(liMenu);
+    };
+
+    return linksContainer;
+
+}
+
+function renderSubject(arr) {
+    for (subject of arr) {
+        const aSubject = document.createElement('a');
+        aSubject.classList.add('subjects');
+    
+        const imgSubject = document.createElement('div');
+        imgSubject.classList.add('subjects-img')
+    
+        const img1Container = document.createElement('div');
+        img1Container.classList.add('img-container');
+    
+        const img1 = document.createElement('img');
+        img1.classList.add('learn-img');
+        img1.setAttribute('alt', subject.name);
+        img1.setAttribute('src', subject.img1);
+        img1.setAttribute('id', 'img1' + subject.name);
+    
+        const img2Container = document.createElement('div');
+        img2Container.classList.add('img-container');
+    
+        const img2 = document.createElement('img');
+        img2.classList.add('learn-img');
+        img2.classList.add('img2');
+        img2.setAttribute('alt', subject.name);
+        img2.setAttribute('src', subject.img2);
+        img2.setAttribute('id', 'img2' + subject.name);
+    
+        const pName = document.createElement('p');
+        pName.classList.add('p-learn');
+        pName.setAttribute('id', 'p' + subject.name);
+        pName.innerText = subject.name;
+    
+        img1Container.appendChild(img1);
+        img2Container.appendChild(img2);
+    
+        imgSubject.appendChild(img1Container);
+        imgSubject.appendChild(img2Container);
+    
+        aSubject.appendChild(imgSubject);
+        aSubject.appendChild(pName);
+    
+        const currentSubject = subject;
+    
+        imgSubject.addEventListener('mouseover', function () {
+            changeImage('img1' + currentSubject.name, 'img2' + currentSubject.name, 'p' + currentSubject.name)
+        });
+    
+        imgSubject.addEventListener('mouseout', function () {
+            restoreImage('img1' + currentSubject.name, 'img2' + currentSubject.name, 'p' + currentSubject.name)
+        });
+    
+        navSubjects.appendChild(aSubject);
+    };
+}
+
+function renderSubmenus(arr) {
+    for (menu of arr) {
+        const liMenu = document.createElement('li');
+        liMenu.classList.add('li-header');
+    
+        const aLiMenu = document.createElement('a');
+        aLiMenu.classList.add('options');
+        aLiMenu.innerText = menu.name;
+    
+        const navLiMenu = document.createElement('nav');
+        navLiMenu.classList.add('desktop-menu');
+        navLiMenu.setAttribute('id', 'nav'+ menu.name)
+    
+        navLiMenu.appendChild(createMenuLinks(menu.links));
+    
+        liMenu.appendChild(aLiMenu);
+        liMenu.appendChild(navLiMenu);
+    
+        const currentMenu = menu;
+    
+        aLiMenu.addEventListener('mouseover', function() {
+            showSubMenu('nav' + currentMenu.name)
+        });
+    
+        aLiMenu.addEventListener('mouseout', function() {
+            hideSubMenu('nav' + currentMenu.name)
+        });
+    
+        navLiMenu.addEventListener('mouseover', function() {
+            showSubMenu('nav' + currentMenu.name)
+        });
+    
+        navLiMenu.addEventListener('mouseout', function() {
+            hideSubMenu('nav' + currentMenu.name)
+        });
+    
+        ulSubMenus.appendChild(liMenu);
+    };
+}
 
 const subjectList = [];
 subjectList.push({
@@ -61,55 +174,47 @@ subjectList.push({
     name: 'Estadística'
 });
 
-for (subject of subjectList) {
-    const aSubject = document.createElement('a');
-    aSubject.classList.add('subjects');
 
-    const imgSubject = document.createElement('div');
-    imgSubject.classList.add('subjects-img')
+const menuList = [];
+menuList.push({
+    name: "Contáctanos",
+    links: [
+        { enlName: "Instagram", enlHref: "/" },
+        { enlName: "Facebook", enlHref: "/" },
+        { enlName: "LinkedIn", enlHref: "/" },
+        { enlName: "Gmail", enlHref: "/" },
+      ],
+});
+menuList.push({
+    name: "Sobre nosotros",
+    links: [
+        { enlName: "Historia", enlHref: "/" },
+        { enlName: "Equipo", enlHref: "/" },
+        { enlName: "Misión y Visión", enlHref: "/" },
+        { enlName: "Reconocimientos", enlHref: "/" },
+      ],
+});
+menuList.push({
+    name: "Noticias",
+    links: [
+        { enlName: "Comunidad", enlHref: "/" },
+        { enlName: "Eventos", enlHref: "/" },
+        { enlName: "Entrevistas", enlHref: "/" },
+        { enlName: "Actualizaciones", enlHref: "/" },
+      ],
+});
+menuList.push({
+    name: "Contenido",
+    links: [
+        { enlName: "Blog", enlHref: "/" },
+        { enlName: "Guías", enlHref: "/" },
+        { enlName: "Preguntas Frecuentes", enlHref: "/" },
+        { enlName: "Asignaturas", enlHref: "/" },
+      ],
+});
 
-    const img1Container = document.createElement('div');
-    img1Container.classList.add('img-container');
+renderSubject(subjectList);
+renderSubmenus(menuList); 
 
-    const img1 = document.createElement('img');
-    img1.classList.add('learn-img');
-    img1.setAttribute('alt', subject.name);
-    img1.setAttribute('src', subject.img1);
-    img1.setAttribute('id', 'img1' + subject.name);
 
-    const img2Container = document.createElement('div');
-    img2Container.classList.add('img-container');
 
-    const img2 = document.createElement('img');
-    img2.classList.add('learn-img');
-    img2.classList.add('img2');
-    img2.setAttribute('alt', subject.name);
-    img2.setAttribute('src', subject.img2);
-    img2.setAttribute('id', 'img2' + subject.name);
-
-    const pName = document.createElement('p');
-    pName.classList.add('p-learn');
-    pName.setAttribute('id', 'p' + subject.name);
-    pName.innerText = subject.name;
-
-    img1Container.appendChild(img1);
-    img2Container.appendChild(img2);
-
-    imgSubject.appendChild(img1Container);
-    imgSubject.appendChild(img2Container);
-
-    aSubject.appendChild(imgSubject);
-    aSubject.appendChild(pName);
-
-    const currentSubject = subject;
-
-    imgSubject.addEventListener('mouseover', function () {
-        changeImage('img1' + currentSubject.name, 'img2' + currentSubject.name, 'p' + currentSubject.name)
-    });
-
-    imgSubject.addEventListener('mouseout', function () {
-        restoreImage('img1' + currentSubject.name, 'img2' + currentSubject.name, 'p' + currentSubject.name)
-    });
-
-    navSubjects.appendChild(aSubject);
-};
