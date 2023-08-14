194195accdbe489d2bb8287e511aa6c310a7881f
Brayan Mendoza <smith0mendoza@gmail.com>
2023-08-13 21:16:32 -0500

    se adapta el tamaño del menu mobile y se cambian unas funciones de sintaxis

--- home/js/script.js
+++ home/js/script.js
@@ -1,27 +1,28 @@
-
+// variables
 const menuIcon = document.querySelector('.icon-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const navSubjects = document.querySelector('.subjects-learn');
 const ulSubMenus = document.querySelector('.ul-header');
 
 
-menuIcon.addEventListener('click', toggleMobileMenu);
 
-function showSubMenu(id) {
+// funciones
+const showSubMenu = id => {
     let subMenu = document.getElementById(id);
     subMenu.style.display = "inline-block";
-
 };
 
-function hideSubMenu(id) {
+const hideSubMenu = id => {
     let subMenu = document.getElementById(id);
     subMenu.style.display = "none";
 };
 
-function toggleMobileMenu() {
+const toggleMobileMenu = () => {
     mobileMenu.classList.toggle('inactive');
     mobileMenu.classList.toggle('active-menu');
+    console.log("quepasa");
 };
+menuIcon.addEventListener('click', toggleMobileMenu);
 
 function changeImage(imgId1, imgId2, pId) {
     document.getElementById(imgId1).style.display = "none";
@@ -56,53 +57,53 @@ function renderSubject(arr) {
     for (subject of arr) {
         const aSubject = document.createElement('a');
         aSubject.classList.add('subjects');
-    
+
         const imgSubject = document.createElement('div');
         imgSubject.classList.add('subjects-img')
-    
+
         const img1Container = document.createElement('div');
         img1Container.classList.add('img-container');
-    
+
         const img1 = document.createElement('img');
         img1.classList.add('learn-img');
         img1.setAttribute('alt', subject.name);
         img1.setAttribute('src', subject.img1);
         img1.setAttribute('id', 'img1' + subject.name);
-    
+
         const img2Container = document.createElement('div');
         img2Container.classList.add('img-container');
-    
+
         const img2 = document.createElement('img');
         img2.classList.add('learn-img');
         img2.classList.add('img2');
         img2.setAttribute('alt', subject.name);
         img2.setAttribute('src', subject.img2);
         img2.setAttribute('id', 'img2' + subject.name);
-    
+
         const pName = document.createElement('p');
         pName.classList.add('p-learn');
         pName.setAttribute('id', 'p' + subject.name);
         pName.innerText = subject.name;
-    
+
         img1Container.appendChild(img1);
         img2Container.appendChild(img2);
-    
+
         imgSubject.appendChild(img1Container);
         imgSubject.appendChild(img2Container);
-    
+
         aSubject.appendChild(imgSubject);
         aSubject.appendChild(pName);
-    
+
         const currentSubject = subject;
-    
+
         imgSubject.addEventListener('mouseover', function () {
             changeImage('img1' + currentSubject.name, 'img2' + currentSubject.name, 'p' + currentSubject.name)
         });
-    
+
         imgSubject.addEventListener('mouseout', function () {
             restoreImage('img1' + currentSubject.name, 'img2' + currentSubject.name, 'p' + currentSubject.name)
         });
-    
+
         navSubjects.appendChild(aSubject);
     };
 }
@@ -111,42 +112,44 @@ function renderSubmenus(arr) {
     for (menu of arr) {
         const liMenu = document.createElement('li');
         liMenu.classList.add('li-header');
-    
+
         const aLiMenu = document.createElement('a');
         aLiMenu.classList.add('options');
         aLiMenu.innerText = menu.name;
-    
+
         const navLiMenu = document.createElement('nav');
         navLiMenu.classList.add('desktop-menu');
-        navLiMenu.setAttribute('id', 'nav'+ menu.name)
-    
+        navLiMenu.setAttribute('id', 'nav' + menu.name)
+
         navLiMenu.appendChild(createMenuLinks(menu.links));
-    
+
         liMenu.appendChild(aLiMenu);
         liMenu.appendChild(navLiMenu);
-    
+
         const currentMenu = menu;
-    
-        aLiMenu.addEventListener('mouseover', function() {
+
+        aLiMenu.addEventListener('mouseover', function () {
             showSubMenu('nav' + currentMenu.name)
         });
-    
-        aLiMenu.addEventListener('mouseout', function() {
+
+        aLiMenu.addEventListener('mouseout', function () {
             hideSubMenu('nav' + currentMenu.name)
         });
-    
-        navLiMenu.addEventListener('mouseover', function() {
+
+        navLiMenu.addEventListener('mouseover', function () {
             showSubMenu('nav' + currentMenu.name)
         });
-    
-        navLiMenu.addEventListener('mouseout', function() {
+
+        navLiMenu.addEventListener('mouseout', function () {
             hideSubMenu('nav' + currentMenu.name)
         });
-    
+
         ulSubMenus.appendChild(liMenu);
     };
 }
 
+
+// listas
 const subjectList = [];
 subjectList.push({
     img1: '../PNG/TADDUME_ICONOS-08.png',
@@ -183,7 +186,7 @@ menuList.push({
         { enlName: "Facebook", enlHref: "/" },
         { enlName: "LinkedIn", enlHref: "/" },
         { enlName: "Gmail", enlHref: "/" },
-      ],
+    ],
 });
 menuList.push({
     name: "Sobre nosotros",
@@ -192,7 +195,7 @@ menuList.push({
         { enlName: "Equipo", enlHref: "/" },
         { enlName: "Misión y Visión", enlHref: "/" },
         { enlName: "Reconocimientos", enlHref: "/" },
-      ],
+    ],
 });
 menuList.push({
     name: "Noticias",
@@ -201,7 +204,7 @@ menuList.push({
         { enlName: "Eventos", enlHref: "/" },
         { enlName: "Entrevistas", enlHref: "/" },
         { enlName: "Actualizaciones", enlHref: "/" },
-      ],
+    ],
 });
 menuList.push({
     name: "Contenido",
@@ -210,11 +213,12 @@ menuList.push({
         { enlName: "Guías", enlHref: "/" },
         { enlName: "Preguntas Frecuentes", enlHref: "/" },
         { enlName: "Asignaturas", enlHref: "/" },
-      ],
+    ],
 });
 
-renderSubject(subjectList);
-renderSubmenus(menuList); 
 
+//ejecuciones de funciones
+renderSubject(subjectList);
+renderSubmenus(menuList);
 
 
